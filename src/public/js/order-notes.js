7ff27db1191169367ff2ca9cef670462051fc699
Dario Masip <masipdario@gmail.com>
2022-01-02 20:51:42 -0300

    Agregue la funcionalidad de agregar y quitar inputs desde el DOM, el guardado de multiples detalles de notas de pedido en un query, detalles en los estilos.

--- src/public/js/order-notes.js
+++ src/public/js/order-notes.js
@@ -1,5 +1,15 @@
 const modal = document.querySelector('.container_modal_order-notes')
 
+const inputs = document.querySelectorAll('.article-and-amount_container')
+const button = document.querySelectorAll('.addInput_button')
+const selects = document.querySelectorAll('#select_article')
+const lastOrderNote = document.querySelectorAll('#lastOrderNote')
+const amount = document.querySelectorAll('#amount')
+const removeInput = document.querySelectorAll('#removeInput')
+
+console.log(inputs.length)
+var index=0;
+
 function openModal() {
     modal.classList.add('is-visible')
     document.addEventListener('keydown', (event) => {
@@ -8,8 +18,64 @@ function openModal() {
         } 
     })
 
+
+    button[0].addEventListener('click',(e)=>{
+        if (index===inputs.length-1){
+            index=0;
+        }
+        inputs[index+1].classList.add('is-display');
+        index++;
+        
+        
+
+
+        
+        
+        selects[index].removeAttribute('disabled')
+        lastOrderNote[index].removeAttribute('disabled')
+        amount[index].removeAttribute('disabled')
+        
+        document.addEventListener('click', (event) =>{
+            
+        if(event.target.firstChild.data=='-'){
+            const button_pressionated = event.target;
+            const NodePadre = button_pressionated.parentNode;
+            NodePadre.classList.remove('is-display');
+            NodePadre.childNodes[1].setAttribute('disabled','')
+            NodeSelect = NodePadre.childNodes[3].childNodes[1];
+            NodeSelect.setAttribute('disabled','')
+            NodeAmount = NodePadre.childNodes[5].childNodes[1];
+            NodeAmount.setAttribute('disabled','')
+            
+            console.log(NodeAmount); 
+        }
+    })
+
+
+        
+        //const clone = input.cloneNode(true)
+        
+        //const childsClone = clone.childNodes;
+        
+        //console.log(childsClone)
+        //containerInputs.appendChild(clone)
+        
+        })
+
 }
 
 function closeModal() {
     modal.classList.remove('is-visible')
+    button[0].removeEventListener;
+
 }
+
+
+
+//const input = document.querySelector('.article-and-amount_container')
+//const containerInputs = document.querySelector('.inputs_articles')
+
+
+
+
+
--- src/routes/links.js
+++ src/routes/links.js
@@ -29,9 +29,13 @@ router.get('/articulos', async (req,res) => {
 
 router.get('/notas-de-pedido', async (req,res) => {
     const orderNotes = await pool.query("SELECT o.`orderNote_id`, o.`voucherNumber`,c.`name`, COUNT(a.`descrition`) AS amount FROM `details-order-note` AS d INNER JOIN `order-notes` AS o ON d.`orderNote_id` = o.`orderNote_id` INNER JOIN `articles` AS a ON a.article_id = d.article_id INNER JOIN `clients` AS c ON c.client_id = o.client_id GROUP BY o.`voucherNumber`;")
+    var lastOrderNote = await pool.query("SELECT `orderNote_id` FROM `order-notes` ORDER BY `orderNote_id` DESC LIMIT 1;")
+    lastOrderNote = Object.values(lastOrderNote[0]);
+    lastOrderNote = lastOrderNote[0] + 1;
     const clients = await pool.query("SELECT `client_id`, `name` FROM clients ORDER BY name ASC LIMIT 150;")
     const articles = await pool.query("SELECT `article_id`, `descrition` FROM articles ORDER BY descrition ASC;")
-    res.render('order-notes', { title:'Notas de pedido - Grupo Max',orderNotes, clients, articles })
+    console.log('LastIrder',lastOrderNote)
+    res.render('order-notes', { title:'Notas de pedido - Grupo Max',orderNotes, clients, articles,lastOrderNote })
 })
 
 router.get('/notas-de-pedido/:id', async (req,res) => {
@@ -43,22 +47,43 @@ router.get('/notas-de-pedido/:id', async (req,res) => {
 router.post('/notas-de-pedido/agregar', async (req,res)=>{
     console.log(req.body)
     const { client_id, voucherNumber } = req.body;
-    const { article_id, amount } = req.body;
+    const { newDetailsOrderNote,newDetailsOrderNote2, newDetailsOrderNote3,newDetailsOrderNote4,newDetailsOrderNote5,newDetailsOrderNote6,
+        newDetailsOrderNote7,
+        newDetailsOrderNote8,newDetailsOrderNote9,newDetailsOrderNote10,newDetailsOrderNote11,
+        newDetailsOrderNote12,newDetailsOrderNote13,newDetailsOrderNote14,newDetailsOrderNote15,newDetailsOrderNote16  } = req.body;
+    var newDetailsOrderNote1 = [ newDetailsOrderNote ]
+    console.log('antes',newDetailsOrderNote1)
+    if (newDetailsOrderNote2) {
+        newDetailsOrderNote1.push(newDetailsOrderNote2)
+        
+    } 
+    if (newDetailsOrderNote3) {
+        newDetailsOrderNote1.push(newDetailsOrderNote3);
+    }
+    if (newDetailsOrderNote4) {
+        newDetailsOrderNote1.push(newDetailsOrderNote4);
+    }
+    if (newDetailsOrderNote5) {
+        newDetailsOrderNote1.push(newDetailsOrderNote5);
+    }
+    if (newDetailsOrderNote6) {
+        newDetailsOrderNote1.push(newDetailsOrderNote6);
+    }
+    if (newDetailsOrderNote7) {
+        newDetailsOrderNote1.push(newDetailsOrderNote7);
+    }
+    console.log('Verdadero',newDetailsOrderNote1)
     const newOrderNote = {
         client_id,
         voucherNumber
     }
     await pool.query('INSERT INTO `order-notes` set ?', [newOrderNote]);
-    const orderNote_IId = await pool.query("SELECT `orderNote_id` FROM `order-notes` ORDER BY `orderNote_id` DESC LIMIT 1;")
-    const orderNoteToJSON = JSON.parse(JSON.stringify(orderNote_IId[0]));
-    const orderNote_id = Object.values(orderNoteToJSON);
-    const newDetailsOrderNote = {
-        orderNote_id,
-        article_id,
-        amount
-      }
-    console.log(orderNote_id)
-    await pool.query('INSERT INTO `details-order-note` set ?',[newDetailsOrderNote])
+    
+    newDetailsOrderNote1 = Object.values(newDetailsOrderNote1)
+
+
+    console.log(newDetailsOrderNote1)
+    await pool.query('INSERT INTO `details-order-note` (`orderNote_id`,`article_id`,`amount`) VALUES ?',[newDetailsOrderNote1])
     res.redirect('/notas-de-pedido');
 })
 
